1300f10d0b26c36e702ec2ce446d80b2ff15db6f
teamthreecoders <teamthreecoders@gmail.com>
2025-10-02 00:21:06 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -24,4 +24,4 @@ def home():
 app.include_router(router)
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
